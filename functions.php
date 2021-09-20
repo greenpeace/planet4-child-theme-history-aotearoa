cf36536119a564c9bcb09ff7e12754ca42e3abd0
Kitt McGregor <kmcgrego@greenpeace.org>
2021-09-20 16:55:46 +1200

    fix syntax error line 90

--- functions.php
+++ functions.php
@@ -87,9 +87,8 @@ add_filter( 'allowed_block_types', 'set_child_theme_allowed_block_types', 15, 2 
 
 add_filter( 'wp_nav_menu_items', 'add_extra_items_to_nav_menu', 10, 2 );
 function add_extra_items_to_nav_menu( $items, $args ) {
-	echo '<li class="nav-item donate-nav-item"><a class="btn btn-donate btn-enhanced-donate" href="https://www.greenpeace.org/aotearoa/act/donate/" data-ga-category="Menu Navigation" data-ga-action="Donate" data-ga-label="Homepage">Donate Now</a></li>'
+	echo '<li class="nav-item donate-nav-item"><a class="btn btn-donate btn-enhanced-donate" href="https://www.greenpeace.org/aotearoa/act/donate/" data-ga-category="Menu Navigation" data-ga-action="Donate" data-ga-label="Homepage">Donate Now</a></li>';
 }
-
 
 function hook_css() {
     ?>
