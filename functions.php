0a09d4f834fe8b209de9a4ec1802b70a0103caf5
Kitt McGregor <kmcgrego@greenpeace.org>
2022-03-24 10:28:13 +1300

    test update navigation.css

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Additional code for the child theme goes in here. March update
+ * Additional code for the child theme goes in here.
  */
 
 add_action( 'wp_enqueue_scripts', 'enqueue_child_styles', 99);
